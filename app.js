3a13636fba50badedeef451271ab9748004200f5
Sarker Sunzid Mahmud <sunzid02@gmail.com>
2025-04-19 15:38:59 +0200

    mvc structured

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog')
+
+const blogRoutes = require('./routes/blogRoutes');
 
 //express app
 const app = express();
@@ -32,72 +33,12 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) =>{
     res.redirect('/blogs');
 });
-
-//blog routes
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1})
-    .then((result) => {
-        res.render('index', {title: 'All Blogs', blogs: result })
-    })
-    .catch((err) => {
-        console.log(err);        
-    })
-});
-
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-    .then((result) => {
-        res.redirect('/blogs');
-    })
-    .catch((err) => {
-        console.log(err);
-        
-    })
-    
-});
-
-
-
-app.get('/about', (req, res) =>{
+app.get('/about', (req, res) => {
     res.render('about', { title: 'About' })
 });
 
-
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create' });
-});
-
-
-app.get('/blogs/:id', (req, res) => {
-
-    const id = req.params.id;
-    Blog.findById(id)
-        .then(result => {
-            res.render('details', { blog: result, title: 'Blog details' });
-        })
-        .catch((err) => {
-            console.log(err);
-
-        })
-    console.log(id);
-
-});
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-        .then((result) => {
-            res.json({ redirect: '/blogs' });
-        })
-        .catch((err) => {
-            console.log(err);
-
-        });
-});
+//blog-routes
+app.use('/blogs', blogRoutes);
 
 
 //404 page
